03f52d170b6e7bb962c77ce8846e17ba8c2f4c7e
Sergio Gayo Pila <aetherwakeestudios@gmail.com>
2025-08-30 04:18:10 +0200

    Fireball!

--- Cards/Lars/Common/Eruption.cs
+++ Cards/Lars/Common/Eruption.cs
@@ -1,133 +1,120 @@
 using Nickel;
 using OneOf.Types;
-using System;
 using System.Collections.Generic;
 using System.Reflection;
-using System.Security.Cryptography.X509Certificates;
 
 namespace AetherWake.LarsMod.Cards;
 
-internal sealed class Fireball : Card, IDemoCard
+internal sealed class Eruption : Card, IDemoCard
 {
     private static ModEntry Instance => ModEntry.Instance;
     public static void Register(IModHelper helper)
     {
-        helper.Content.Cards.RegisterCard("Fireball", new()
+        helper.Content.Cards.RegisterCard("Eruption", new()
         {
             CardType = MethodBase.GetCurrentMethod()!.DeclaringType!,
             Meta = new()
             {
                 deck = ModEntry.Instance.Lars_Deck.Deck,
 
-                rarity = Rarity.rare,
+                rarity = Rarity.common,
 
                 upgradesTo = [Upgrade.A, Upgrade.B]
             },
             
-            Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "Fireball", "name"]).Localize
+            Name = ModEntry.Instance.AnyLocalizations.Bind(["card", "Eruption", "name"]).Localize
         });
     }
+    
+    private int GetX(State s)
+	{
+        var x = s.ship.Get(Status.heat);
+		return x;
+	}
 
-    public override CardData GetData(State s)
+    public override CardData GetData(State state)
     {
         CardData data = new();
-        switch(upgrade){
+        switch (upgrade)
+        {
             case Upgrade.None:
                 data = new CardData()
                 {
-                    cost = 2,
+                    cost = 1,
                 };
                 break;
             case Upgrade.A:
                 data = new CardData()
                 {
-                    cost = 2,
-                    infinite=true
+                    cost = 0
                 };
                 break;
             case Upgrade.B:
-                data = new CardData(){
-                    cost = 2,
+                data = new CardData()
+                {
+                    cost = 1
                 };
                 break;
         }
         return data;
     }
-
-    private int GetX(Combat c)
-	{
-        var x = c.hand.Count;
-		return x;
-	}
-
     public override List<CardAction> GetActions(State s, Combat c)
     {
-        
         List<CardAction> actions = new();
+
         switch (upgrade)
         {
             case Upgrade.None:
                 actions = new()
                 {
-                    new AStatus(){ status=Status.drawLessNextTurn, statusAmount = 2, targetPlayer= true },
                     new AVariableHint
                     {
-                        hand=true,
+                        status=  Status.heat,
                     },
                     new AAttack(){
-                        damage = GetX(c),
-                        xHint = 1
-                    },
-                    new AAttack(){
-                        damage = GetX(c),
+                        damage = GetX(s),
                         xHint = 1
                     },
                     new AStatus(){
-                        xHint = 1,
-                        status=Status.heat, statusAmount = GetX(c)
+                        status=Status.heat, statusAmount=-1
                     }
                 };
                 break;
             case Upgrade.A:
                 actions = new()
                 {
-                    new AStatus(){ status=Status.drawLessNextTurn, statusAmount = 2, targetPlayer= true },
+                    new AStatus(){
+                        status=Status.heat, statusAmount=1
+                    },
                     new AVariableHint
                     {
-                        hand=true,
+                        status=  Status.heat,
                     },
                     new AAttack(){
-                        damage = GetX(c),
-                        xHint = 1
-                    },
-                    new AAttack(){
-                        damage = GetX(c),
+                        damage = GetX(s),
                         xHint = 1
                     },
                     new AStatus(){
-                        xHint = 1,
-                        status=Status.heat, statusAmount = GetX(c), targetPlayer=true
+                        status=Status.heat, statusAmount=-2
                     }
                 };
                 break;
             case Upgrade.B:
                 actions = new()
                 {
-                    new AStatus(){ status=Status.drawLessNextTurn, statusAmount = 2, targetPlayer= true },
+                    new AStatus(){
+                        status=Status.heat, statusAmount=2
+                    },
                     new AVariableHint
                     {
-                        hand=true,
+                        status=  Status.heat,
                     },
                     new AAttack(){
-                        damage = GetX(c),
-                        xHint = 2
+                        damage = GetX(s),
+                        xHint = 1
                     },
                     new AStatus(){
-                        xHint = 1,
-                        status=Status.heat, statusAmount = GetX(c), targetPlayer=true
-                    },
-                    new AStatus(){
-                        status=Status.heat, statusAmount = -1, targetPlayer = true
+                        status=Status.heat, statusAmount=-1
                     }
                 };
                 break;
